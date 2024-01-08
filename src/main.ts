df1689f00d154b0d6b730557b89dc51ab2a09cee
Alan Jiang <adj2424@gmail.com>
2024-01-08 01:43:35 -0500

    it is done

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import { gsap } from 'gsap';
 import { ScrollTrigger, ScrollToPlugin } from 'gsap/all';
-// import SplitType from 'split-type';
+import SplitType from 'split-type';
 import Cursor from './components/cursor.ts';
 import Hero from './components/hero.ts';
 import Works from './components/works.ts';
@@ -9,91 +9,108 @@ import Technologies from './components/technologies.ts';
 import Contact from './components/contact.ts';
 import { darkColor } from './colors.ts';
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-// let count = 100;
-// let loadPercent = 0;
-
-/**
- * Loading screen
- */
-// function load() {
-// 	// finished loading and play enter animation
-// 	if (count >= 100) {
-// 		gsap.to('#load-name', {
-// 			fontSize: '30px',
-// 			top: '2.5%',
-// 			left: '5%',
-// 			duration: 1.5,
-// 			ease: 'power2.out',
-// 			delay: 0.6
-// 		});
-// 		// wait for .5 seconds
-// 		setTimeout(() => {
-// 			let loadingPercent = new SplitType('#loading');
-// 			gsap
-// 				.timeline()
-// 				.to(loadingPercent!.chars, {
-// 					yPercent: -200,
-// 					stagger: 0.15,
-// 					duration: 0.5,
-// 					delay: 0.5
-// 				})
-// 				.to('.loader-container', {
-// 					opacity: 0,
-// 					duration: 0.5,
-// 					delay: 0.3,
-// 					ease: 'power2.out'
-// 				})
-// 				.add(() => {
-// 					document.querySelector('.loader-container')?.remove();
-// 				});
-// 		}, 500);
-// 		return;
-// 	}
-// 	// must wait for load manager to finish
-// 	if (count / 100 >= loadPercent) {
-// 		setTimeout(load, 60);
-// 	} else {
-// 		count++;
-// 	}
-// 	const loading = document.getElementById('loading')!;
-// 	loading.innerHTML = count + '%';
-// 	let scalePercent = (count * 82) / 100;
-// 	document.getElementById('loading')!.style.right = 82 - scalePercent + '%';
-// 	setTimeout(load, 60);
-// }
-
-// artificial loading
-//load();
-document.querySelector('.loader-container')?.remove();
-
 /**
  * Initialize meshes
  */
-let isMobile = window.innerWidth < 550;
-
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+const isMobile = window.innerWidth < 550;
+isMobile ? document.querySelector('.loader-container')?.remove() : load(0);
+resize();
 new Cursor(isMobile);
 new Hero();
 new Works();
 new Technologies().init(isMobile);
 new Contact(isMobile);
 
-document.getElementById('header-name')!.addEventListener('click', () => {
-	scrollToPosition(0);
+window.addEventListener('resize', () => {
+	resize();
 });
-// document.getElementById('header-about')!.addEventListener('click', () => {
-// 	scrollToPosition(0.11);
-// });
+
+document.getElementById('header-name')!.addEventListener('click', () => {
+	scrollToPosition(0.01);
+});
 document.getElementById('header-works')!.addEventListener('click', () => {
-	scrollToPosition(0.37);
+	scrollToPosition(0.355);
 });
 document.getElementById('header-technologies')!.addEventListener('click', () => {
-	scrollToPosition(0.71);
+	scrollToPosition(0.69);
 });
 document.getElementById('header-contact')!.addEventListener('click', () => {
-	scrollToPosition(0.9);
+	scrollToPosition(0.91);
 });
+
+/**
+ * Recursive loading screen
+ */
+function load(count: number) {
+	// finished loading and play enter animation
+	if (count >= 100) {
+		const header = document.getElementById('header-name')!;
+		const font = window.getComputedStyle(header).fontSize;
+		const { top, left } = header.getBoundingClientRect();
+		gsap.to('#load-name', {
+			fontSize: font,
+			top: top,
+			left: left,
+			duration: 1.5,
+			ease: 'power2.out',
+			delay: 0.5
+		});
+
+		// wait for .5 seconds
+		setTimeout(() => {
+			let loadingPercent = new SplitType('#loading', { types: 'chars' });
+			gsap
+				.timeline()
+				.to(loadingPercent!.chars, {
+					yPercent: -200,
+					stagger: 0.15,
+					duration: 0.5,
+					delay: 0.5
+				})
+				.to('.loader-container', {
+					opacity: 0,
+					duration: 0.5,
+					delay: 0.1,
+					ease: 'power2.out'
+				})
+				.add(() => {
+					document.querySelector('.loader-container')?.remove();
+				});
+		}, 500);
+		return;
+	}
+	const d = Math.random();
+	if (d > 0.66) {
+		count += 3;
+	} else if (d > 0.33) {
+		count += 2;
+	} else {
+		count += 1;
+	}
+	if (count > 100) {
+		count = 100;
+	}
+	const loading = document.getElementById('loading')!;
+	loading.innerHTML = `${count}%`;
+	let scalePercent = (count * 82) / 100;
+	document.getElementById('loading')!.style.right = 82 - scalePercent + '%';
+	setTimeout(load, 40, count);
+}
+
+function resize() {
+	const headerLeft = document.getElementsByClassName('header-left')[0] as HTMLElement;
+	const headerRight = document.getElementsByClassName('header-right')[0] as HTMLElement;
+	if (isMobile || window.innerWidth < 600) {
+		headerRight.style.visibility = 'hidden';
+		headerLeft.style.width = '87.5%';
+		headerRight.style.minWidth = window.innerWidth < 450 ? '100%' : '450px';
+	} else {
+		headerRight.style.visibility = 'visible';
+		headerLeft.style.width = '';
+		headerRight.style.width = '25%';
+	}
+}
 
 const scrollToPosition = (percent: number) => {
 	let scrollContentHeight = document.querySelector('.page')!.scrollHeight;
@@ -105,80 +122,6 @@ const scrollToPosition = (percent: number) => {
 		scrollTo: scrollContentHeight * percent
 	});
 };
-const resize = () => {
-	const headerLeft = document.getElementsByClassName('header-left')[0] as HTMLElement;
-	const headerRight = document.getElementsByClassName('header-right')[0] as HTMLElement;
-	if (isMobile || window.innerWidth < 600) {
-		headerLeft.style.visibility = 'hidden';
-		headerRight.style.width = '100%';
-		headerRight.style.marginRight = '0';
-		headerRight.style.minWidth = window.innerWidth < 450 ? '100%' : '450px';
-	} else {
-		headerLeft.style.visibility = 'visible';
-		headerRight.style.width = '30%';
-	}
-};
-resize();
-window.addEventListener('resize', () => {
-	resize();
-});
-
-// const aboutHello = new SplitType('.about-hello');
-// const aboutText = new SplitType('.about-text', { types: 'lines' });
-const aboutLines = document.querySelectorAll('.about-text .line');
-
-createLineWrapper(aboutLines);
-//createLineWrapper(projectNameLines);
-// wrap lines with div fsr to make line stagger work idk
-function createLineWrapper(lines: NodeListOf<Element>) {
-	lines.forEach(line => {
-		const wrapper = document.createElement('div');
-		wrapper.classList.add('line-wrapper');
-		line.parentNode!.insertBefore(wrapper, line);
-		wrapper.appendChild(line);
-	});
-}
-
-/**
- * scroll animation by toggle
- */
-// about
-// gsap.fromTo(
-// 	aboutHello.chars,
-// 	{
-// 		yPercent: 300
-// 	},
-// 	{
-// 		scrollTrigger: {
-// 			trigger: '.about-container',
-// 			start: '210% center',
-// 			end: '300% 0%',
-// 			//markers: true,
-// 			toggleActions: 'play reverse play reverse'
-// 		},
-// 		yPercent: 0,
-// 		stagger: 0.1,
-// 		duration: 0.4
-// 	}
-// );
-// gsap.fromTo(
-// 	aboutText.lines,
-// 	{
-// 		yPercent: 100
-// 	},
-// 	{
-// 		scrollTrigger: {
-// 			trigger: '.about-container',
-// 			start: '210% center',
-// 			end: '300% 0%',
-// 			//markers: true,
-// 			toggleActions: 'play reverse play reverse'
-// 		},
-// 		yPercent: 0,
-// 		stagger: 0.1,
-// 		duration: 0.2
-// 	}
-// );
 
 /**
  * https://tympanus.net/codrops/2022/12/13/how-to-code-an-on-scroll-folding-3d-cardboard-box-animation-with-three-js-and-gsap/
@@ -234,17 +177,16 @@ timeline
 	.to('#right', { xPercent: 50, duration: 5 }, 89)
 
 	// transition to contact page
-	.to('.technologies-container', { xPercent: -100, duration: 20 }, 85)
+	.to('.technologies-container', { xPercent: -100, duration: 20 }, 87)
 	.to('.main', { x: 0, duration: 20 }, 85)
-	.to('.contact-container', { xPercent: -100, duration: 20 }, 85)
-	.to('.contact-container', { color: darkColor.hexString, duration: 2 }, 85)
-	.to('#header-contact', { color: darkColor.hexString, duration: 1.5 }, 85.5)
-	.to('#header-technologies', { color: darkColor.hexString, duration: 1.5 }, 86.5)
-	.to('#header-works', { color: darkColor.hexString, duration: 1.5 }, 87.66)
-	// .to('#header-about', { color: darkColor.hexString, duration: 1.5 }, 89)
-	.to('#header-name', { color: darkColor.hexString, duration: 4 }, 96.5)
+	.to('.contact-container', { xPercent: -100, duration: 20 }, 87)
+	.to('.contact-container', { color: darkColor.hexString, duration: 2 }, 87)
+	.to('#header-contact', { color: darkColor.hexString, duration: 1.5 }, 87.5)
+	.to('#header-technologies', { color: darkColor.hexString, duration: 1.5 }, 88.25)
+	.to('#header-works', { color: darkColor.hexString, duration: 1.5 }, 89.3)
+	.to('#header-name', { color: darkColor.hexString, duration: 8 }, 98)
 
-	// to make start time a percentage out of 140 from total duration
-	// start time + duration cannot be greater than 140 or it will change timeline
+	// to make start time a percentage out of 110 from total duration
+	// start time + duration cannot be greater than 110 or it will change timeline
 	.to({}, {}, 110);
 // it was 100% at 1500vh
