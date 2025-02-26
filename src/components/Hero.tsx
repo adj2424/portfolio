dec28b3d60cfc1a1fa1105551b596e00e24c42f9
Alan <adj2424@gmail.com>
2025-02-26 17:10:08 -0500

    b2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,14 @@
 export const Hero = () => {
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center">
-        <div>pic</div>
-        <img src="/public/pfp.jpg" />
-        <div className="flex absolute bg-green-600 w-full bottom-[1rem] justify-around">
+      <div className="relative w-full h-screen flex items-center justify-center">
+        <img className="w-[25rem]" src="/pfp.jpg" />
+        <div className="absolute flex">
+          <div className="mr-[.5rem]">ALAN JIANG</div>
+          <div>ALAN JIANG</div>
+        </div>
+
+        <div className="flex absolute bg-green-600 w-[90%] bottom-[4rem] justify-between text-[1.3rem]">
           <div>SOFTWARE ENGINEER</div>
           <div>SCROLL</div>
           <div>FULL STACK DEVELOPER</div>
